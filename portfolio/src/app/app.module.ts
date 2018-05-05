6368d0b0e54274c2d146c5556a35bf435a8b5da4
marcelpierres <marcelpierre.samuels@ryerson.ca>
2018-05-05 15:06:38 -0400

    Working Home Redirect

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    HomeComponent,
+    ContactMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      { path: '',   redirectTo: '/home', pathMatch: 'full' },
+      {path: 'contact-me', component: ContactMeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
